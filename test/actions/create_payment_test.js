b7a6d4b34c6b7a4cb98b42a77887e3d717f0b5fb
Marlon Casagranda <marlondiego407@gmail.com>
2018-10-15 15:39:45 -0300

    Implementado testes para actions

---
+++ test/actions/create_payment_test.js
@@ -0,0 +1,43 @@
+const plg = require('pluga-plg');
+const expect = require('chai').expect;
+const action = require('../../lib/actions/create_payment');
+require('dotenv').config()
+
+const event = {
+  meta:{
+    baseURI: process.env.BASE_URI
+  },
+  auth: {
+    api_key: process.env.api_key
+  },
+  input:{
+      customer: {
+        name: "Marcelo Almeida",
+        email: "marcelo.almeida@gmail.com",
+        phone: "4738010919",
+        mobilePhone: "4799376637",
+        cpfCnpj: "84627446527",
+        postalCode: "01310-000",
+        address: "Av. Paulista",
+        addressNumber: "150",
+        complement: "Sala 201",
+        province: "Centro",
+        externalReference: "12987382"
+      },
+      billingType: "BOLETO",
+      dueDate: "2019-06-10",
+      value: 100,
+      description: "Pedido 056984",
+      externalReference: "056984"
+  }
+};
+
+describe('Action: Create payment', () => {
+  it('Should create a new payment', function (done) {
+    action.handle(plg, event).then(result => {
+      expect(result).to.have.property("id");
+
+      done();
+    }).catch(done);
+  });
+});
