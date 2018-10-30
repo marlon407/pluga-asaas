1256f9932ef90369d937c3597bbddb277d2513a9
juniormesquitadandao <juniormesquitadandao@gmail.com>
2018-10-30 15:39:31 -0300

    refactor

--- test/triggers/payment_created_test.js
+++ test/triggers/payment_created_test.js
@@ -9,7 +9,7 @@ describe('Trigger: payment created', () => {
         baseURI: process.env.BASE_URI
       },
       auth: {
-        api_key: process.env.api_key
+        api_key: process.env.API_KEY
       },
       payload: {"object":"payment","id":"pay_445582527532","dateCreated":"2018-10-19","customer":"cus_000000018615","value":33.33,"netValue":26.37,"originalValue":null,"interestValue":null,"description":"","billingType":"BOLETO","status":"PENDING","dueDate":"2018-10-23","originalDueDate":"2018-10-23","paymentDate":null,"clientPaymentDate":null,"invoiceUrl":"https://sandbox.asaas.com/i/445582527532","invoiceNumber":"00148303","externalReference":null,"deleted":false,"anticipated":false,"creditDate":null,"estimatedCreditDate":null,"bankSlipUrl":"https://sandbox.asaas.com/b/pdf/445582527532","lastInvoiceViewedDate":null,"lastBankSlipViewedDate":null,"discount":{"value":0.00,"limitDate":null,"dueDateLimitDays":0,"type":"PERCENTAGE"},"fine":{"value":0.00,"type":"PERCENTAGE"},"interest":{"value":0.00,"type":"PERCENTAGE"}}
     };
@@ -29,7 +29,7 @@ describe('Trigger: payment created', () => {
         baseURI: process.env.BASE_URI
       },
       auth: {
-        api_key: process.env.api_key
+        api_key: process.env.API_KEY
       },
       payload: {"object":"payment","id":"pay_445582527532","dateCreated":"2018-10-19","customer":"cus_123","value":33.33,"netValue":26.37,"originalValue":null,"interestValue":null,"description":"","billingType":"BOLETO","status":"PENDING","dueDate":"2018-10-23","originalDueDate":"2018-10-23","paymentDate":null,"clientPaymentDate":null,"invoiceUrl":"https://sandbox.asaas.com/i/445582527532","invoiceNumber":"00148303","externalReference":null,"deleted":false,"anticipated":false,"creditDate":null,"estimatedCreditDate":null,"bankSlipUrl":"https://sandbox.asaas.com/b/pdf/445582527532","lastInvoiceViewedDate":null,"lastBankSlipViewedDate":null,"discount":{"value":0.00,"limitDate":null,"dueDateLimitDays":0,"type":"PERCENTAGE"},"fine":{"value":0.00,"type":"PERCENTAGE"},"interest":{"value":0.00,"type":"PERCENTAGE"}}
     };
