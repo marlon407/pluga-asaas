48c1799738a9136a6e9b704868ffce8df001f890
Marlon Casagranda <marlondiego407@gmail.com>
2018-11-08 09:02:57 -0200

    Adicionado no retorno do webhook taxa cobrada pelo Asaas

--- test/triggers/payment_created_test.js
+++ test/triggers/payment_created_test.js
@@ -11,12 +11,13 @@ describe('Trigger: payment created', () => {
       auth: {
         api_key: process.env.api_key
       },
-      payload: {"object":"payment","id":"pay_445582527532","dateCreated":"2018-10-19","customer":"cus_000000018615","value":33.33,"netValue":26.37,"originalValue":null,"interestValue":null,"description":"","billingType":"BOLETO","status":"PENDING","dueDate":"2018-10-23","originalDueDate":"2018-10-23","paymentDate":null,"clientPaymentDate":null,"invoiceUrl":"https://sandbox.asaas.com/i/445582527532","invoiceNumber":"00148303","externalReference":null,"deleted":false,"anticipated":false,"creditDate":null,"estimatedCreditDate":null,"bankSlipUrl":"https://sandbox.asaas.com/b/pdf/445582527532","lastInvoiceViewedDate":null,"lastBankSlipViewedDate":null,"discount":{"value":0.00,"limitDate":null,"dueDateLimitDays":0,"type":"PERCENTAGE"},"fine":{"value":0.00,"type":"PERCENTAGE"},"interest":{"value":0.00,"type":"PERCENTAGE"}}
+      payload: {"object":"payment","id":"pay_445582527532","dateCreated":"2018-10-19","customer":"cus_000000018662","value":33.33,"netValue":26.37,"originalValue":null,"interestValue":null,"description":"","billingType":"BOLETO","status":"PENDING","dueDate":"2018-10-23","originalDueDate":"2018-10-23","paymentDate":null,"clientPaymentDate":null,"invoiceUrl":"https://sandbox.asaas.com/i/445582527532","invoiceNumber":"00148303","externalReference":null,"deleted":false,"anticipated":false,"creditDate":null,"estimatedCreditDate":null,"bankSlipUrl":"https://sandbox.asaas.com/b/pdf/445582527532","lastInvoiceViewedDate":null,"lastBankSlipViewedDate":null,"discount":{"value":0.00,"limitDate":null,"dueDateLimitDays":0,"type":"PERCENTAGE"},"fine":{"value":0.00,"type":"PERCENTAGE"},"interest":{"value":0.00,"type":"PERCENTAGE"}}
     };
 
     trigger.handle(plg, event).then((payment) => {
       expect(payment).to.have.property("id");
       expect(payment.status).to.eq("PENDING");
+      expect(payment.asaasFee).to.eq(6.96);
       expect(payment.customer).to.have.property("id");
 
       done();
